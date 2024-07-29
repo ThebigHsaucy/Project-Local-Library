43d107f583934717a66b30720b71dac58ff2bb6f
Hayden Gartzman <158302444+ThebigHsaucy@users.noreply.github.com>
2024-07-29 08:32:12 -0400

    Complete qualified assignment,...

---
+++ docs/function printName(name){.js
@@ -0,0 +1,8 @@
+function printName(name){
+    2  console.log("Hello " + name);
+    3}
+    4
+    5setTimeout(printName, 3000, "Thundercat");
+    6setTimeout(printName, 2000, "Adele");
+    7setTimeout(printName, 1000, "Sade");
+
